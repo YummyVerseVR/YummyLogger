1834f5bb2b4bda8885cb45b8613118e90defefd1
uPiscium <upiscium@gmail.com>
2025-10-23 10:49:45 +0900

    Setup

---
+++ src/entry.py
@@ -0,0 +1,18 @@
+import argparse
+import uvicorn
+from pylognet.server import LoggingService
+
+
+parser = argparse.ArgumentParser(description="Run the FastAPI application.")
+parser.add_argument(
+    "-p",
+    "--port",
+    type=int,
+    default=9000,
+    help="Port to run the FastAPI application on",
+)
+args = parser.parse_args()
+
+if __name__ == "__main__":
+    service = LoggingService()
+    service.run(port=args.port)
