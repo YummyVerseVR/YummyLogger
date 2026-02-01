2f43a49799099ebde37645e918c08004ce423990
uPiscium <upiscium@gmail.com>
2026-02-02 07:40:19 +0900

    Add config file and update entry script

--- src/entry.py
+++ src/entry.py
@@ -1,4 +1,5 @@
 import argparse
+import json
 from pylognet.server import LoggingService
 
 
@@ -7,10 +8,23 @@ parser.add_argument(
     "-p",
     "--port",
     type=int,
-    default=9000,
+    default=None,
     help="Port to run the FastAPI application on",
 )
+parser.add_argument(
+    "-c",
+    "--config",
+    type=str,
+    default="settings/config.json",
+    help="Path to the configuration file",
+)
 args = parser.parse_args()
+
+with open(args.config, "r") as f:
+    config = json.load(f)
+
+if args.port is None:
+    args.port = config.get("system", {}).get("port", 9000)
 
 if __name__ == "__main__":
     service = LoggingService()
